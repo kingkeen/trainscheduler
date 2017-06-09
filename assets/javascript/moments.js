ad46839f38b0801eca7edc742021842e9e368c2b
Joaquin Ignacio <Keen@Revolt.local>
2017-06-08 20:41:16 -0700

    huge updates to CSS and HTML file

---
+++ assets/javascript/moments.js
@@ -0,0 +1,19 @@
+
+
+PSEUDOCODE FOR THE TRAINS
+
+departure time = start time
+
+arrive time = start time plus duration
+
+from where? 
+
+to where? 
+
+--------
+
+listen for document load then do the rest. 
+
+
+
+
