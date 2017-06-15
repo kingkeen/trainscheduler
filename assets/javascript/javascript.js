7bc74efbd7b7a7e45414e7aff6efc5febccc74be
Joaquin Ignacio <Keen@Revolt.local>
2017-06-14 17:16:15 -0700

    minor edits

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,42 @@
+// SET VARIABLES
+// - ORIGIN CITY
+// - DESTINATION CITY
+// - START TIME
+// - FREQUENTY MINUTES
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAoYwpwB4awwoJv98Nw6q8lGFeNhZTl60M",
+    authDomain: "trainscheduler-96a50.firebaseapp.com",
+    databaseURL: "https://trainscheduler-96a50.firebaseio.com",
+    projectId: "trainscheduler-96a50",
+    storageBucket: "trainscheduler-96a50.appspot.com",
+    messagingSenderId: "582229155503"
+  };
+  
+  firebase.initializeApp(config);
+
+
+// Create variable to take the string of information from the submit. 
+
+// Load the string of information from the submit to Firebase
+
+// Create code to check if there are firebase data. If not, then listen for Submit.
+
+// If firebase data, then load the database data to for-loop
+
+// use the for-loop to generate the HTML to load the information to the table
+
+
+
+
+
+
+
+// INITIALIZE WHEN DOCUMENT IS READY THE CALL FOR THE SUBMIT BUTTON
+// VALIDATE THE SUBMIT BUTTON HAS ALL FIELDS FILLED IN. 
+
+// VALIDATE THE TIME ENTERED IS IN CORRECT FORMAT
+
+
