c29b37c123bc5eb77beb255934bdb8607cafdcaf
Joaquin Ignacio <Keen@Revolt.local>
2017-06-15 21:56:52 -0700

    large changes to JS and Firebase

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,7 +6,7 @@
 
 
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyAoYwpwB4awwoJv98Nw6q8lGFeNhZTl60M",
     authDomain: "trainscheduler-96a50.firebaseapp.com",
     databaseURL: "https://trainscheduler-96a50.firebaseio.com",
@@ -15,7 +15,49 @@
     messagingSenderId: "582229155503"
   };
   
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+var database = firebase.database();
+
+// listens for the train submit button
+$("#newSubmission").on("click", function(event) {
+	event.preventDefault();
+
+	// Grabs the fields input
+	var trainOrigin = $("#train-origin").val().trim();
+	var trainDest = $("#train-destination").val().trim();
+	var startTime = $("#train-firstTime").val().trim();
+	var trainFreq = $("#train-frequency").val().trim();
+
+	// console.log("train-origin: ", trainOrigin);
+	// console.log("train-destination: ", trainDest);
+	// console.log("train-firstTime: ", startTime);
+	// console.log("train-frequency: ", trainFreq);
+
+	var newTrain = {
+		origin: trainOrigin,
+		trainDestination: trainDest,
+		startingTime: startTime,
+		frequency: trainFreq
+	};
+
+	// pushes to firebase the new train input
+	database.ref().push(newTrain);
+
+	// console.log(newTrain.origin);
+	// console.log(newTrain.trainDestination);
+	// console.log(newTrain.startingTime);
+	// console.log(newTrain.frequency);
+
+	alert("Train has been added to schedule");
+
+	// Clears the input fields
+	$("#train-origin").val("");
+	$("#train-destination").val("");
+	$("#train-firstTime").val("");
+	$("#train-frequency").val("");
+
+});
+
 
 
 // Create variable to take the string of information from the submit. 
